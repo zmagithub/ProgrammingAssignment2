6d91f7e01d430fa00e792ac5144636dd211a6771
Zafer <zma.github@gmail.com>
2021-01-03 20:28:44 -0800

    Basic function definitions were added as required by the assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,9 @@
-## Put comments here that give an overall description of what your
-## functions do
+
+## The following pair of functions are used to cache the inverse of a matrix.he following pair of functions are used to cache the inverse of a matrix.
 
 ## Write a short comment describing this function
+
+# The function makeCacheMatrix: creates a special matrix object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 
@@ -9,6 +11,11 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+
+# The function cacheSolve: computes the inverse of the special matrix returned by the makeCacheMatrix.  
+
+# If the inverse has already been calculated the function should skip the computation 
+# and retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
